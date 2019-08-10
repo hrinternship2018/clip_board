f046f797253f094c8e4f5c635e059f01de312a2e
Ruofan He <hrinternship2018@gmail.com>
2019-08-09 22:36:21 -0700

    Change files

--- src/expressServer.ts
+++ src/expressServer.ts
@@ -2,10 +2,24 @@ import express from 'express'
 
 export function init(){
     const app = express()
+    let content = ''
 
     app.get('/', function (req, res) {
-        res.send('Hello World')
+        res.sendFile(process.cwd() + '/page/index.html')
     })
+
+    app.post('/post', function (req, res) {
+        console.log(req.headers.text)
+        content = (req.headers.text || '') as string
+        res.send('posted')
+    })
+
+    app.get('/data', function (req, res) {
+        console.log('sended')
+        res.set('Content-Type', 'text/plain');
+        res.send(content)
+    })
+
     app.listen(3000)
 
 }
